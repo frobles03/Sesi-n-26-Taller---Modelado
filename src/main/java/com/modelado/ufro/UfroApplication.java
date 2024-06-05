ccc35224a939de618f9f44da451705dc791d6657
f_robles03 <64452088+frobles03@users.noreply.github.com>
2024-06-05 14:56:22 -0400

    base proeycto
    se creo la base del proyecto

---
+++ src/main/java/com/modelado/ufro/UfroApplication.java
@@ -0,0 +1,13 @@
+package com.modelado.ufro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfroApplication.class, args);
+	}
+
+}
